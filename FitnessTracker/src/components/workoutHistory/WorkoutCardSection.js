92689ce4bdcda95cb5a4aadec02182d3aad59e86
gudbrandschistad <gschistad@dons.usfca.edu>
2018-11-27 17:19:13 -0800

    Fixed bug in workout history where the text went outside of the card. And removed whitespaces in analytics page http requests

--- FitnessTracker/src/components/workoutHistory/WorkoutCardSection.js
+++ FitnessTracker/src/components/workoutHistory/WorkoutCardSection.js
@@ -21,9 +21,8 @@ class WorkoutCardSection extends Component {
   
 
   render() {
-    console.log(this.props.exercise.categoryurl)
     return (
-      <View style={{flex: 1}}>
+      <View styles={styles.container}>
        <ExerciseItem
             imageurl={this.props.exercise.categoryurl}
             workoutName={this.props.exercise.workoutname}
@@ -41,69 +40,6 @@ export default WorkoutCardSection;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-  },
-  cardStyle: {
-      flexDirection: 'column',
-      justifyContent:'center',
-      paddingTop: 20
-  },
-  topCardWrapperStyle: {
-      flexDirection:'row',
-      justifyContent:'center'
-    },
-    WorkoutNameStyle: {
-      justifyContent: 'flex-start',
-      flex:1,
-      paddingLeft: 10
-    },
-    plusSignStyle: {
-      justifyContent: "flex-end", // main axis
-      paddingRight: 10,
-      flex: 0
-
-    },
-    inputFieldsWrapper: {
-      margin: 10,
-      flex:1,
-      flexDirection: "row",
-      justifyContent: 'space-between',
-  
-    },
-    cardExpandStyle: {
-      paddingTop: 20,
-      borderTopWidth: 1,
-      borderColor: '#ddd',
-    },
-    cardCollapseStyle: {
-      paddingTop: 0,
-      borderTopWidth: 0,
-    },
-  inputField: {
-      borderRadius: 4,
-      borderWidth: 1,
-      padding: 5,
-      margin: 5,
-      flex: 1,
-      alignItems: 'stretch',
-      width: 90
-
-  },
-  missingFieldColor: {
-      borderColor: 'red',
-  },
-  normalFieldColor: {
-      borderColor: '#d6d7da',
-  },
-  labelStyle: {
-      textAlign: 'center'
-  },    
-  WorkoutNameTextStyle: {
-      fontWeight: '200',
-      color: '#636463',
-      fontSize: 16,
-      fontFamily:'arial', 
   },
 });
 
